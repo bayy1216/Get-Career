a0305b0370de88dcaa5f93e387150ee0a7d26c43
Hong <bayyyy@naver.com>
2023-10-03 14:33:38 +0900

    [Feat]: CareerModel에 company 속성 추가

--- app/src/main/java/com/reditus/getcareer/data/dto/CareerModel.kt
+++ app/src/main/java/com/reditus/getcareer/data/dto/CareerModel.kt
@@ -2,7 +2,17 @@ package com.reditus.getcareer.data.dto
 
 data class CareerModel(
     val id: String,
+    val company: Company,
     val url: String,
     val title: String,
     val date: String,
 )
+
+enum class Company {
+    NAVER,
+    KAKAO,
+    LINE,
+    COUPANG,
+    BAEMIN,
+    OTHER
+}
